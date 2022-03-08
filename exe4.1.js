2f0759368cdacc7230d2a01e62634b694d935a45
rannerdepaula <rannerdepaula@gmail.com>
2022-03-08 18:44:29 -0300

    exe4.1.js

---
+++ exe4.1.js
@@ -0,0 +1,83 @@
+/*const a = 3
+const b = 2
+
+console.log('Adicao')
+let adicao = a + b
+console.log(adicao)
+
+console.log('Subtracao')
+let subtracao = a + b
+console.log(subtracao)
+
+console.log('Multiplicacao')
+let multiplicacao = a * b
+console.log(multiplicacao)
+
+
+console.log('Divisao')
+let divisao = a / b
+console.log(divisao)
+
+console.log('Modulo')
+let modulo = a % b
+console.log(modulo) */
+
+/*let b = 56
+let c = 20
+let d = 8
+
+if (b > c & b > d) {
+    console.log('O Maior número é'+ b)
+} else if(c > b & c > d) {
+    console.log('O Maior número é'+ b)
+} else {
+    console.log('O Maior número é' + d)
+}
+
+const a = 0
+
+if (a > 0) {
+    console.log(a) + console.log('é positivo')
+} else if (a < 0) {
+    console.log(a) + console.log('é negativo')
+} else {
+    console.log('Vale zero')
+}
+
+const a = 200
+const b = -110
+const c = 90
+const soma = a + b +c 
+
+
+if ( soma === 180 & a > 0 & b > 0 & c > 0) {
+    console.log('true')
+} else {
+    console.log('False')
+} 
+
+const a = 3
+const b = 5
+const c = 11
+
+if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+    console.log('True')
+} else {
+    console.log('False')
+}*/
+
+const cp = 5 - 9
+const vp = 10
+const ip = 0.2
+const vc = cp + cp*ip
+const l = vp - vc
+const lcem = l * 100000 
+
+
+
+if (l > 0 & cp > 0 & vp > 0 & ip > 0 & vc > 0 & lcem > 0 ) {
+    console.log(`O Lucro da Empresa foi R$${lcem}`)
+} else {
+    console('[ERRO] Revise seus valores de entrada')
+}
+
